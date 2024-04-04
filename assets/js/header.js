bca287954dfe88203fd4c714d377eab0aec47469
nancy-dalal16 <nancydalal16@gmail.com>
2024-04-04 15:10:05 +0530

    small content changes

--- assets/js/header.js
+++ assets/js/header.js
@@ -13,3 +13,35 @@ mobile_nav.addEventListener("click", () => toggleNavbar(
 
 
 
+
+(function(document){
+    var div = document.getElementById('servicemenu');
+   
+    var icon = document.getElementById('icon');
+    var open = false;
+    
+    div.addEventListener('click', function(){
+      if(open){
+        icon.className = 'fa fa-arrow-down';  
+      } else{
+        icon.className = 'fa fa-arrow-down open';
+      }
+      
+      open = !open;
+    });
+  })(document);
+
+
+
+
+// $('#servicemenu').click(function() {
+    // icon = $(this).find("i");
+    // if( icon.hasClass("bx bx-md bx-chevron-down"))
+    // {
+    //     icon.addClass("bx bx-md bx-chevron-up").removeClass("bx bx-md bx-chevron-down");
+    // }
+    // else{
+    //     icon.addClass("bx bx-md bx-chevron-down").removeClass("bx bx-md bx-chevron-up");
+    //   }
+  
+// });
